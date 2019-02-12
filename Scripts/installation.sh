632e90cf1435fcabcd96b99127465af304d8cc26
BaihongQi <bq@ualberta.ca>
2019-02-12 12:51:55 -0700

    script for installation

---
+++ Scripts/installation.sh
@@ -0,0 +1,10 @@
+pip install pandas
+pip install numpy
+pip install pvlib
+python -mpip install matplotlib
+pip install -U scikit-learn
+sudo apt update
+sudo apt install python3-dev python3-pip
+sudo pip3 install -U virtualenv  # system-wide install
+pip install cvxpy
+pip install gym
