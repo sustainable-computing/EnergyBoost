9422b415a4a4fc76f74ff981ccc52a6caa5de492
Abdullah-al-Zishan <azishan@ualberta.ca>
2019-12-27 09:46:38 -0700

    Bulk Processes

---
+++ NewBoost/SAC/process_59_20_135_1440_1512.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6  # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --time=00-14:00      # time (DD-HH:MM)
+#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+#SBATCH --error=%N-%j.err   # %N for node name, %j for jobID
+#SBATCH --job-name=SAC_59_20_135_1440_1512
+
+module load python/3.6.3
+module load scipy-stack
+virtualenv --no-download ~/ENV
+source ~/ENV/bin/activate
+pip install torch --no-index
+pip install tqdm
+
+python main.py 0.2 13.5 5 ../data/added_hhdata_59_2.csv 1440 1512
---
+++ NewBoost/SAC/process_59_4_64_720_792.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6  # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --time=00-14:00      # time (DD-HH:MM)
+#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+#SBATCH --error=%N-%j.err   # %N for node name, %j for jobID
+#SBATCH --job-name=SAC_59_4_64_720_792
+
+module load python/3.6.3
+module load scipy-stack
+virtualenv --no-download ~/ENV
+source ~/ENV/bin/activate
+pip install torch --no-index
+pip install tqdm
+
+python main.py 0.04 6.4 2 ../data/added_hhdata_59_2.csv 720 792
